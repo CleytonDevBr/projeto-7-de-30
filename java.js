2aa186c5346de09cccf6a3f463a3db23a213ae61
CleytonDevBr <CleytonDevs@gmail.com>
2022-06-24 12:27:51 -0300

    22

--- java.js
+++ java.js
@@ -1,12 +1,17 @@
-const botao = document.getElementById('btn')
+const btnMobile = document.getElementById('btn-menu');
 
-function abrirMenu(){
-  const nav= document.getElementById('nav')
-  nav.classList.toggle('active')
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
 }
 
-
-
-
-/* O BUTTON VAI TER UM EVENTO QUANDO CLICADO */
-botao.addEventListener('click', abrirMenu)
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu); 
